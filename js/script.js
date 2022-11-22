a778a9ae9d2000bc942baa6358e39e2aad8c0395
mohamedkante <114979734+mohamedkante@users.noreply.github.com>
2022-11-22 10:14:14 +0100

    Fin des pays MED

--- js/script.js
+++ js/script.js
@@ -1,33 +1,71 @@
 const refresh = () => {
-	setTimeout('theFunction()', 10000)
+	setTimeout('fonctionGlobale()', 10000)
 }
+refresh()
 
 const fonctionGlobale = () => {
 
+	let date = new Date()
+	let hour = date.getHours()
+	console.log(hour)
+
+	// Partie de MED
 	//Debut de la partie à copier
-	//Nom du Pays : Nigeria
-	fetch('https://api.open-meteo.com/v1/gfs?latitude=10.00&longitude=8.00&hourly=temperature_2m&forecast_days=1')
+	//Nom du Pays : Venezuela
+	fetch('https://api.open-meteo.com/v1/gfs?latitude=8.00&longitude=-66.00&hourly=temperature_2m&forecast_days=1')
 	.then(response => response.json())
 	.then(temp => {
 		tempa = temp.hourly.temperature_2m
-		let date = new Date()
-		let hour = date.getHours()
-		console.log(hour)
 		for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
-				document.getElementById('nigeria').innerHTML = "Nigeria's temperature " + tempa[i]
-				refresh()
+				document.getElementById('Venezuela').innerHTML = "Venezuela 's temperature " + tempa[i]
 			}
 		}
-		})
-		//Fin de la partie à copier
+		}
+		)
+		//Fin de la premèere partie
+
+		//Nom du Pays : Colombia
+       
+	fetch('https://api.open-meteo.com/v1/gfs?latitude=4.00&longitude=-73.25&hourly=temperature_2m&forecast_days=1')
+	.then(response => response.json())
+	.then(temp => {
+		tempa = temp.hourly.temperature_2m
+		for (let i = 0 ; i < tempa.length; i++){
+			if (i === hour){
+				document.getElementById('Colombia').innerHTML = "Colombia 's temperature " + tempa[i]
+			}
+		}
+		}
+		)
+		
+
+		///Nom du Pays : Guyana
+
+	fetch('https://api.open-meteo.com/v1/gfs?latitude=5.00&longitude=-59.00&hourly=temperature_2m&forecast_days=1')
+	.then(response => response.json())
+	.then(temp => {
+		tempa = temp.hourly.temperature_2m
+		for (let i = 0 ; i < tempa.length; i++){
+			if (i === hour){
+				document.getElementById('Guyana').innerHTML = "Guyana 's temperature " + tempa[i]
+			}
+		}
+		}
+		)
+		//Nom du Pays : Ecuador
+		fetch('https://api.open-meteo.com/v1/gfs?latitude=-1.25&longitude=-78.25&hourly=temperature_2m&forecast_days=1')
+		.then(response => response.json())
+		.then(temp => {
+			tempa = temp.hourly.temperature_2m
+			for (let i = 0 ; i < tempa.length; i++){
+				if (i === hour){
+					document.getElementById('Ecuador').innerHTML = "Ecuador 's temperature " + tempa[i]
+				}
+			}
+			}
+			)
+			
+		// Fin de la partie de MED
 	}
-	fonctionGlobale()
-
-
-
-	//fetch('https://api.open-meteo.com/v1/gfs?latitude=39.74&longitude=-104.98&hourly=temperature_2m&forecast_days=1')
-	//.then(response => response.json())
-	//.then(response => console.log(response.hourly.temperature_2m))
-
-
+	 fonctionGlobale()
