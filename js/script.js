16ea1f132ba4084a87b35990962931b65c6bbe2d
HortenseLaclaviere <114992888+HortenseLaclaviere@users.noreply.github.com>
2022-11-23 15:06:34 +0100

    ajout des couleurs selon température

--- js/script.js
+++ js/script.js
@@ -7,18 +7,33 @@ const fonctionGlobale = () => {
 
 	let date = new Date()
 	let hour = date.getHours()
-	console.log(hour)
+	console.log("heure : ", hour)
 
 	//Nom du Pays : Venezuela
   
 	fetch('https://api.open-meteo.com/v1/gfs?latitude=8.00&longitude=-66.00&hourly=temperature_2m&forecast_days=1')
   .then(response => response.json())
 	.then(temp => {
-		tempa = temp.hourly.temperature_2m
+		let tempa = temp.hourly.temperature_2m
 		for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
-      document.getElementById('Venezuela').innerHTML = "Venezuela 's temperature " + tempa[i]
-			}
+			
+      document.getElementById('Venezuela').innerHTML = tempa[i];
+	 
+	 				 if (tempa[i]<(-10)){
+						document.getElementById('Venezuela').style.color='rgb(33, 15, 148)';
+					 } else if (tempa[i]<= 0) {
+						document.getElementById('Venezuela').style.color='rgb(132, 213, 253)';
+					 } else if (tempa[i]<= 10) {
+						document.getElementById('Venezuela').style.color='rgb(237, 253, 132)';
+					 }else if (tempa[i]<= 20) {
+						document.getElementById('Venezuela').style.color='rgb(247, 156, 65)';
+					 }else if (tempa[i]<= 29) {
+						document.getElementById('Venezuela').style.color='rgb(233, 30, 23)';
+					 }else if (tempa[i]>= 30) {
+						document.getElementById('Venezuela').style.color='rgb(146, 3, 3)';
+					 }
+	 			}
 		}
 		})
     
@@ -31,6 +46,20 @@ const fonctionGlobale = () => {
 		for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
 				document.getElementById('peru').innerHTML = "Peru's temperature " + tempa[i]
+				
+				if (tempa[i]<(-10)){
+					document.getElementById('peru').style.color='rgb(33, 15, 148)';
+				 } else if (tempa[i]<= 0) {
+					document.getElementById('peru').style.color='rgb(132, 213, 253)';
+				 } else if (tempa[i]<= 10) {
+					document.getElementById('peru').style.color='rgb(237, 253, 132)';
+				 }else if (tempa[i]<= 20) {
+					document.getElementById('peru').style.color='rgb(247, 156, 65)';
+				 }else if (tempa[i]<= 29) {
+					document.getElementById('peru').style.color='rgb(233, 30, 23)';
+				 }else if (tempa[i]>= 30) {
+					document.getElementById('peru').style.color='rgb(146, 3, 3)';
+				 }
 				}
 		}
 		})
@@ -43,7 +72,21 @@ const fonctionGlobale = () => {
 		tempa = temp.hourly.temperature_2m
 		for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
-				document.getElementById('uruguay').innerHTML = "Uruguay's temperature " + tempa[i]		
+				document.getElementById('uruguay').innerHTML = "Uruguay's temperature " + tempa[i]
+				
+				if (tempa[i]<(-10)){
+					document.getElementById('uruguay').style.color='rgb(33, 15, 148)';
+				 } else if (tempa[i]<= 0) {
+					document.getElementById('uruguay').style.color='rgb(132, 213, 253)';
+				 } else if (tempa[i]<= 10) {
+					document.getElementById('uruguay').style.color='rgb(237, 253, 132)';
+				 }else if (tempa[i]<= 20) {
+					document.getElementById('uruguay').style.color='rgb(247, 156, 65)';
+				 }else if (tempa[i]<= 29) {
+					document.getElementById('uruguay').style.color='rgb(233, 30, 23)';
+				 }else if (tempa[i]>= 30) {
+					document.getElementById('uruguay').style.color='rgb(146, 3, 3)';
+				 }		
 			}
 		}
 		})
@@ -57,6 +100,20 @@ const fonctionGlobale = () => {
 		for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
 				document.getElementById('brazil').innerHTML = "Brazil's temperature " + tempa[i]	
+				
+				if (tempa[i]<(-10)){
+					document.getElementById('brazil').style.color='rgb(33, 15, 148)';
+				 } else if (tempa[i]<= 0) {
+					document.getElementById('brazil').style.color='rgb(132, 213, 253)';
+				 } else if (tempa[i]<= 10) {
+					document.getElementById('brazil').style.color='rgb(237, 253, 132)';
+				 }else if (tempa[i]<= 20) {
+					document.getElementById('brazil').style.color='rgb(247, 156, 65)';
+				 }else if (tempa[i]<= 29) {
+					document.getElementById('brazil').style.color='rgb(233, 30, 23)';
+				 }else if (tempa[i]>= 30) {
+					document.getElementById('brazil').style.color='rgb(146, 3, 3)';
+				 }
 			}
 		}
 		})
@@ -70,6 +127,20 @@ const fonctionGlobale = () => {
 				for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
 				document.getElementById('frenchGuyana').innerHTML = "French Guyana's temperature " + tempa[i]
+				
+				if (tempa[i]<(-10)){
+					document.getElementById('frenchGuyana').style.color='rgb(33, 15, 148)';
+				 } else if (tempa[i]<= 0) {
+					document.getElementById('frenchGuyana').style.color='rgb(132, 213, 253)';
+				 } else if (tempa[i]<= 10) {
+					document.getElementById('frenchGuyana').style.color='rgb(237, 253, 132)';
+				 }else if (tempa[i]<= 20) {
+					document.getElementById('frenchGuyana').style.color='rgb(247, 156, 65)';
+				 }else if (tempa[i]<= 29) {
+					document.getElementById('frenchGuyana').style.color='rgb(233, 30, 23)';
+				 }else if (tempa[i]>= 30) {
+					document.getElementById('frenchGuyana').style.color='rgb(146, 3, 3)';
+				 }
 			}
 		}
 		})
@@ -83,6 +154,20 @@ const fonctionGlobale = () => {
 				for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
 				document.getElementById('suriname').innerHTML = "Suriname's temperature " + tempa[i]
+				
+				if (tempa[i]<(-10)){
+					document.getElementById('suriname').style.color='rgb(33, 15, 148)';
+				 } else if (tempa[i]<= 0) {
+					document.getElementById('suriname').style.color='rgb(132, 213, 253)';
+				 } else if (tempa[i]<= 10) {
+					document.getElementById('suriname').style.color='rgb(237, 253, 132)';
+				 }else if (tempa[i]<= 20) {
+					document.getElementById('suriname').style.color='rgb(247, 156, 65)';
+				 }else if (tempa[i]<= 29) {
+					document.getElementById('suriname').style.color='rgb(233, 30, 23)';
+				 }else if (tempa[i]>= 30) {
+					document.getElementById('suriname').style.color='rgb(146, 3, 3)';
+				 }
 			}
 		}
 		})
@@ -96,6 +181,20 @@ const fonctionGlobale = () => {
 		for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
 				document.getElementById('bolivia').innerHTML = "Bolivia's temperature " + tempa[i]
+				
+				if (tempa[i]<(-10)){
+					document.getElementById('bolivia').style.color='rgb(33, 15, 148)';
+				 } else if (tempa[i]<= 0) {
+					document.getElementById('bolivia').style.color='rgb(132, 213, 253)';
+				 } else if (tempa[i]<= 10) {
+					document.getElementById('bolivia').style.color='rgb(237, 253, 132)';
+				 }else if (tempa[i]<= 20) {
+					document.getElementById('bolivia').style.color='rgb(247, 156, 65)';
+				 }else if (tempa[i]<= 29) {
+					document.getElementById('bolivia').style.color='rgb(233, 30, 23)';
+				 }else if (tempa[i]>= 30) {
+					document.getElementById('bolivia').style.color='rgb(146, 3, 3)';
+				 }
 			}
 		}
 		})
@@ -109,6 +208,20 @@ const fonctionGlobale = () => {
 		for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
 				document.getElementById('paraguay').innerHTML = "Paraguay's temperature " + tempa[i]
+				
+				if (tempa[i]<(-10)){
+					document.getElementById('paraguay').style.color='rgb(33, 15, 148)';
+				 } else if (tempa[i]<= 0) {
+					document.getElementById('paraguay').style.color='rgb(132, 213, 253)';
+				 } else if (tempa[i]<= 10) {
+					document.getElementById('paraguay').style.color='rgb(237, 253, 132)';
+				 }else if (tempa[i]<= 20) {
+					document.getElementById('paraguay').style.color='rgb(247, 156, 65)';
+				 }else if (tempa[i]<= 29) {
+					document.getElementById('paraguay').style.color='rgb(233, 30, 23)';
+				 }else if (tempa[i]>= 30) {
+					document.getElementById('paraguay').style.color='rgb(146, 3, 3)';
+				 }
 			}
 		}
 		})
@@ -122,6 +235,20 @@ const fonctionGlobale = () => {
 		for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
 				document.getElementById('chile').innerHTML = "Chile's temperature " + tempa[i]
+				
+				if (tempa[i]<(-10)){
+					document.getElementById('chile').style.color='rgb(33, 15, 148)';
+				 } else if (tempa[i]<= 0) {
+					document.getElementById('chile').style.color='rgb(132, 213, 253)';
+				 } else if (tempa[i]<= 10) {
+					document.getElementById('chile').style.color='rgb(237, 253, 132)';
+				 }else if (tempa[i]<= 20) {
+					document.getElementById('chile').style.color='rgb(247, 156, 65)';
+				 }else if (tempa[i]<= 29) {
+					document.getElementById('chile').style.color='rgb(233, 30, 23)';
+				 }else if (tempa[i]>= 30) {
+					document.getElementById('chile').style.color='rgb(146, 3, 3)';
+				 }
 			}
 		}
 		})
@@ -135,6 +262,20 @@ const fonctionGlobale = () => {
 		for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
 				document.getElementById('argentina').innerHTML = "Argentina's temperature " + tempa[i]
+				
+				if (tempa[i]<(-10)){
+					document.getElementById('argentina').style.color='rgb(33, 15, 148)';
+				 } else if (tempa[i]<= 0) {
+					document.getElementById('argentina').style.color='rgb(132, 213, 253)';
+				 } else if (tempa[i]<= 10) {
+					document.getElementById('argentina').style.color='rgb(237, 253, 132)';
+				 }else if (tempa[i]<= 20) {
+					document.getElementById('argentina').style.color='rgb(247, 156, 65)';
+				 }else if (tempa[i]<= 29) {
+					document.getElementById('argentina').style.color='rgb(233, 30, 23)';
+				 }else if (tempa[i]>= 30) {
+					document.getElementById('argentina').style.color='rgb(146, 3, 3)';
+				 }
 			}
 		}
 		})
@@ -148,6 +289,20 @@ const fonctionGlobale = () => {
 		for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
 				document.getElementById('Colombia').innerHTML = "Colombia 's temperature " + tempa[i]
+				
+				if (tempa[i]<(-10)){
+					document.getElementById('Colombia').style.color='rgb(33, 15, 148)';
+				 } else if (tempa[i]<= 0) {
+					document.getElementById('Colombia').style.color='rgb(132, 213, 253)';
+				 } else if (tempa[i]<= 10) {
+					document.getElementById('Colombia').style.color='rgb(237, 253, 132)';
+				 }else if (tempa[i]<= 20) {
+					document.getElementById('Colombia').style.color='rgb(247, 156, 65)';
+				 }else if (tempa[i]<= 29) {
+					document.getElementById('Colombia').style.color='rgb(233, 30, 23)';
+				 }else if (tempa[i]>= 30) {
+					document.getElementById('Colombia').style.color='rgb(146, 3, 3)';
+				 }
 			}
 		}
 		})
@@ -157,10 +312,24 @@ const fonctionGlobale = () => {
 	fetch('https://api.open-meteo.com/v1/gfs?latitude=5.00&longitude=-59.00&hourly=temperature_2m&forecast_days=1')
 	.then(response => response.json())
 	.then(temp => {
-		tempa = temp.hourly.temperature_2m
+		let tempa = temp.hourly.temperature_2m
 		for (let i = 0 ; i < tempa.length; i++){
 			if (i === hour){
 				document.getElementById('Guyana').innerHTML = "Guyana 's temperature " + tempa[i]
+				
+				if (tempa[i]<(-10)){
+					document.getElementById('Guyana').style.color='rgb(33, 15, 148)';
+				 } else if (tempa[i]<= 0) {
+					document.getElementById('Guyana').style.color='rgb(132, 213, 253)';
+				 } else if (tempa[i]<= 10) {
+					document.getElementById('Guyana').style.color='rgb(237, 253, 132)';
+				 }else if (tempa[i]<= 20) {
+					document.getElementById('Guyana').style.color='rgb(247, 156, 65)';
+				 }else if (tempa[i]<= 29) {
+					document.getElementById('Guyana').style.color='rgb(233, 30, 23)';
+				 }else if (tempa[i]>= 30) {
+					document.getElementById('Guyana').style.color='rgb(146, 3, 3)';
+				 }
 			}
 		}
 		})
@@ -174,8 +343,29 @@ const fonctionGlobale = () => {
 			for (let i = 0 ; i < tempa.length; i++){
 				if (i === hour){
 					document.getElementById('Ecuador').innerHTML = "Ecuador 's temperature " + tempa[i]
+					
+					if (tempa[i]<(-10)){
+						document.getElementById('Ecuador').style.color='rgb(33, 15, 148)';
+					 } else if (tempa[i]<= 0) {
+						document.getElementById('Ecuador').style.color='rgb(132, 213, 253)';
+					 } else if (tempa[i]<= 10) {
+						document.getElementById('Ecuador').style.color='rgb(237, 253, 132)';
+					 }else if (tempa[i]<= 20) {
+						document.getElementById('Ecuador').style.color='rgb(247, 156, 65)';
+					 }else if (tempa[i]<= 29) {
+						document.getElementById('Ecuador').style.color='rgb(233, 30, 23)';
+					 }else if (tempa[i]>= 30) {
+						document.getElementById('Ecuador').style.color='rgb(146, 3, 3)';
+					 }
 				}
 			}
 			})
 	}
-  fonctionGlobale()
+	fonctionGlobale()
+
+	function changeColor(A){
+
+		if(A<10){
+			A.style.color=rgb(55, 101, 185);
+		}else{A.style.color=rgb(22, 37, 143);}
+		  }
